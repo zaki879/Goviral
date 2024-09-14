7ec5a26369f0d1f03d909744965fdda9509f5a48
zaki879 <zaki.bouchebtoul@univ-constantine2.dz>
2024-09-14 13:38:34 +0100

    push f

--- secondmain.js
+++ secondmain.js
@@ -1261,56 +1261,7 @@ function initCardsIntro(e) {
   });
 }
 function initCardsHover() {
-  document.querySelectorAll("[data-card]").forEach((e) => {
-    const t = e.style.zIndex || 0;
-    if (!0 === ("static" === e.getAttribute("data-card"))) return;
-    const o = e.querySelector("video"),
-      r = e.querySelector("img");
-    e.addEventListener("mouseenter", () => {
-      (e.style.zIndex = 2),
-        gsap.to(e, {
-          scale: prefersReducedMotion() ? 1 : 1.15,
-          rotate: prefersReducedMotion() ? 0 : 16 * Math.random() - 8,
-          duration: 0.6,
-          ease: CustomEase.create(
-            "guides-bounce",
-            "M0,0 C0.084,0.61 0.202,0.898 0.327,0.977 0.555,1.121 0.661,0.92 1,1 "
-          ),
-        }),
-        supportsTouch() ||
-          (gsap.to(r, { opacity: 0, duration: 0.2, ease: "power2" }),
-          gsap.to(o, {
-            opacity: 1,
-            duration: 0.2,
-            ease: "power2",
-            onComplete: () => {
-              o.play();
-            },
-          }));
-    }),
-      e.addEventListener("mouseleave", () => {
-        (e.style.zIndex = t),
-          gsap.to(e, {
-            scale: 1,
-            rotate: prefersReducedMotion() ? 0 : 6 * Math.random() - 3,
-            duration: 0.6,
-            ease: CustomEase.create(
-              "guides-bounce",
-              "M0,0 C0.084,0.61 0.202,0.898 0.327,0.977 0.555,1.121 0.661,0.92 1,1 "
-            ),
-          }),
-          supportsTouch() ||
-            (gsap.to(r, { opacity: 1, duration: 0.2, ease: "power2" }),
-            gsap.to(o, {
-              opacity: 0,
-              duration: 0.2,
-              ease: "power2",
-              onComplete: () => {
-                o.pause();
-              },
-            }));
-      });
-  });
+ 
 }
 function initHomeParallax() {
   const e = document.querySelector('[data-parallax="trigger"]'),
